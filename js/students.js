716c866cdd476357f1d7caa2f1a67e81722676de
Nikita Popov <losnikitos@gmail.com>
2013-09-30 03:31:40 +0400

    Empty profile picture now comes from Liga web symbols font. Students.json added

--- js/students.js
+++ js/students.js
@@ -1,6 +1,12 @@
 function StudentsCtrl($scope) {
-    $scope.students = [
-        {first_name: 'Никита', last_name: 'Попов'},
-        {first_name: 'Василий', last_name: 'Теркин'}
-    ]
+    var jsonPath = "../data/students.json";
+
+    $.getJSON(jsonPath, function (data) {
+        $scope.students = data;
+    });
+
+//    $scope.students = [
+//        {first_name: 'Никита', last_name: 'Попов'},
+//        {first_name: 'Василий', last_name: 'Теркин'}
+//    ]
 }
