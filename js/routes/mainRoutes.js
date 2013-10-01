1b769a1e54ff31f94e33185d4f7766d464ffca73
Nikita Popov <losnikitos@gmail.com>
2013-10-01 16:43:14 +0400

    Revert "Attempt to use Require.js. Not successful though"
    This reverts commit d64d211ce1612f084703bc5993fe30ff1c9b811a.

--- js/routes/mainRoutes.js
+++
@@ -1,6 +0,0 @@
-define(['modules/mainApp' , 'controllers/listCtrl'] , function (mainApp) {
-    return mainApp.config(['$routeProvider' , function ($routeProvider) {
-        $routeProvider.when('/' , {controller: 'listCtrl' , templateUrl: '/templates/List.html'});
-    }]);
-
-});
--- js/students.js
+++ js/students.js
@@ -1,12 +1,12 @@
 function StudentsCtrl($scope) {
     var jsonPath = "../data/students.json";
 
-//    $.getJSON(jsonPath, function (data) {
-//        $scope.students = data;
-//    });
+    $.getJSON(jsonPath, function (data) {
+        $scope.students = data;
+    });
 
-    $scope.students = [
-        {first_name: 'Никита', last_name: 'Попов'},
-        {first_name: 'Василий', last_name: 'Теркин'}
-    ]
+//    $scope.students = [
+//        {first_name: 'Никита', last_name: 'Попов'},
+//        {first_name: 'Василий', last_name: 'Теркин'}
+//    ]
 }
