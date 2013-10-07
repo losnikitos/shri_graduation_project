db4ab28fa27d3260753dbfea0177904ae32cdcf3
Nikita Popov <losnikitos@gmail.com>
2013-10-08 03:17:53 +0400

    Design stuff: new logo, parallax effect

--- js/app.js
+++ js/app.js
@@ -1,32 +1,30 @@
-angular.module('shri', []).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.
+//angular.module('shri', []).
+//    config(['$routeProvider', function ($routeProvider) {
+//        $routeProvider.
+//
+//            when('/', {controller:StudentsCtrl, templateUrl:'list.html'}).
+////            when('/edit/:studentID', {controller: EditStudentCtrl, templateUrl:'parts/editStudent.html'}).
+//
+//            when('/students', {templateUrl: 'parts/students.html', controller: StudentsCtrl}).
+//            when('/students/:studentID', {templateUrl: 'parts/studentDetails.html', controller: StudentDetailsCtrl}).
+//
+//            when('/courses', {templateUrl: 'parts/courses.html', controller: CoursesCtrl}).
+//            when('/courses/:courseID', {templateUrl: 'parts/courseDetails.html', controller: CourseDetailsCtrl}).
+//            otherwise({redirectTo: '/'})}]);
+//
+//
 
-            when('/students', {templateUrl: 'parts/students.html', controller: StudentsCtrl}).
-            when('/students/:studentID', {templateUrl: 'parts/studentDetails.html', controller: StudentDetailsCtrl}).
-
-            when('/courses', {templateUrl: 'parts/courses.html', controller: CoursesCtrl}).
-            when('/courses/:courseID', {templateUrl: 'parts/courseDetails.html', controller: CourseDetailsCtrl}).
-            otherwise({redirectTo: '/students'});
-    }]);
 
 $(function () {
-//    $('.app-folders-container').appFolders();
-    console.log('Дом построен');
-    $.getJSON('data/students.json', function (data) {
-        this.window["students"] = data
-    });
-
     //parallax scrolling
     var bg = $(".jumbotron");
-    $(window).scroll(function(e){
+    $(window).scroll(function (e) {
         parallaxScroll(bg);
     });
 });
 
-function parallaxScroll(el)
-{
+function parallaxScroll(el) {
     var yPos = -($(window).scrollTop() / 2) - 50;
-    var coords = 'center '+ yPos + 'px';
+    var coords = 'center ' + yPos + 'px';
     el.css({ backgroundPosition: coords });
 }
--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,7 @@
 function StudentsCtrl($scope, $http) {
-    $scope.students = window.students;
+    $http.get('data/students.json').success(function(data) {
+        $scope.students = data;
+    });
 };
 
 function StudentDetailsCtrl($scope, $routeParams) {
@@ -8,3 +10,36 @@ function StudentDetailsCtrl($scope, $routeParams) {
 
 function CoursesCtrl($scope, $http) {};
 function CourseDetailsCtrl($scope, $http) {};
+
+
+
+
+
+
+function EditStudentCtrl($scope, $location, $routeParams, Project) {
+    var self = this;
+
+    //Using studentID from URL. Ain't it RESTful?
+    Student.get({id: $routeParams.studentId}, function(student) {
+        self.original = student;
+        $scope.student = new Student(self.original);
+    });
+
+    //To enable or disable 'Save' button
+    $scope.isClean = function() {
+        return angular.equals(self.original, $scope.student);
+    }
+
+    //For erasing a student
+    $scope.destroy = function() {
+        self.original.destroy(function() {
+            $location.path('/list');
+        });
+    };
+
+    $scope.save = function() {
+        $scope.student.update(function() {
+            $location.path('/');
+        });
+    };
+}
